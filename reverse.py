1b1ff12f2a9377506946b286abc224a2bc7825de
Bandipoojitha <bandipoojitha7702@gmail.com>
2026-01-28 16:21:17 +0530

    reverse added

---
+++ reverse.py
@@ -0,0 +1,17 @@
+''' write a program to find the revrse of the given number'''
+def reverse(num):
+    rev = 0
+    while num>0:
+        rev = rev *10+num%10
+    num//=10
+    return rev
+
+
+def isPalindrome(num):
+    return num==reverse(num)
+
+print(reverse(123))
+print(isPalindrome(123))
+
+print(reverse(121))
+print(isPalindrome(121))
